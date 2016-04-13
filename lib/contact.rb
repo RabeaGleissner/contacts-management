4722113ff91e254d59ca927e679bb53032617ee2
Rabea Gleissner <rabeagleissner@gmail.com>
2016-04-13 18:11:50 +0100

    introduces contact with all data fields

--- lib/contact.rb
+++ lib/contact.rb
@@ -1,2 +1,11 @@
 class Contact
+  attr_reader :first_name, :last_name, :email, :mobile, :twitter
+
+  def initialize(first_name, last_name, email, mobile, twitter)
+    @first_name = first_name
+    @last_name = last_name
+    @email = email
+    @mobile = mobile
+    @twitter = twitter
+  end
 end
--- spec/contact_spec.rb
+++ spec/contact_spec.rb
@@ -2,5 +2,7 @@ require 'spec_helper'
 require 'contact'
 
 describe Contact do
-
+  it "has a first name, last name, email, mobile number and twitter handle" do
+    contact = Contact.new("first", "last", "email", "0292929", "twitter")
+  end
 end
