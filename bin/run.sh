c9a198a5887d8409eb259e265558fbe95eb86325
Rabea Gleissner <rabeagleissner@gmail.com>
2016-04-14 16:44:42 +0100

    deletes unused classes

--- bin/run.sh
+++ bin/run.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env ruby
 $LOAD_PATH << File.expand_path("../../lib", __FILE__)
 
-require 'contact'
 require 'ui'
 require 'app'
 App.new(Ui.new($stdin, $stdout)).run
--- spec/contact_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-require 'contact'
-
-describe Contact do
-  it "has a first name, last name, email, mobile number and twitter handle" do
-    contact = Contact.new("first", "last", "email", "0292929", "twitter")
-  end
-end
