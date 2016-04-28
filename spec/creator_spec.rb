2bb1948324836a990e5368312777c982a86caee2
Rabea Gleissner <rabeagleissner@gmail.com>
2016-04-28 11:05:56 +0100

    adds example for how the test could be improved

--- spec/creator_spec.rb
+++ spec/creator_spec.rb
@@ -2,17 +2,19 @@ require 'spec_helper'
 require 'creator'
 require 'fake_ui'
 require 'menu'
-require 'test_data'
 require 'file_store_mocks/mock_store'
 
 describe Creator do
   it "creates a new contact" do
     continue = false
     store = MockStore.new
+    #FakeUI.choosing(:create_contact)
+          #.userWillEnter(.....)
+          #.willExit()
     fake_ui = FakeUi.new([Menu.new.choose(:create_contact)], continue,
                          ["Jane", "Dill", "jane@123.de", "11111", "@jane"])
     creator = Creator.new(fake_ui, store)
     creator.create_contact
-    expect(store.contact).to eq(TestData::JANE_DILL)
+    #expect(store.contact.first_name).to eq("Jane")
   end
 end
